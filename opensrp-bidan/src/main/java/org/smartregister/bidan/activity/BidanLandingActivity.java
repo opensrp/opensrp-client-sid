c893f4aa164ca474f0e94b5bf8ba55c74ee44ae2
baksosapi <wildan.dev@gmail.com>
2017-11-07 14:48:42 +0800

    WD|Fix using CustomFontTextView. #4

--- opensrp-bidan/src/main/java/org/smartregister/bidan/activity/BidanLandingActivity.java
+++ opensrp-bidan/src/main/java/org/smartregister/bidan/activity/BidanLandingActivity.java
@@ -3,21 +3,38 @@ package org.smartregister.bidan.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import org.smartregister.bidan.R;
+import org.smartregister.bidan.toolbar.LocationSwitcherToolbar;
+import org.smartregister.view.customcontrols.CustomFontTextView;
 
 /**
  * Created by sid-tech on 11/7/17.
  */
 
-public class BidanLandingActivity extends AppCompatActivity{
+public class BidanLandingActivity extends AppCompatActivity {
 
+    private MenuItem updateMenuItem;
+    private MenuItem remainingFormsToSyncMenuItem;
+    private TextView ecRegisterClientCountView;
+    private TextView kartuIbuANCRegisterClientCountView;
+    private TextView kartuIbuPNCRegisterClientCountView;
+    private TextView anakRegisterClientCountView;
+    private TextView kohortKbCountView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.bidan_landing);
+
+        ecRegisterClientCountView = (TextView) findViewById(R.id.txt_kartu_ibu_register_client_count);
+        kartuIbuANCRegisterClientCountView = (TextView) findViewById(R.id.txt_kartu_ibu_anc_register_client_count);
+        kartuIbuPNCRegisterClientCountView = (TextView) findViewById(R.id.txt_kartu_ibu_pnc_register_client_count);
+        anakRegisterClientCountView = (TextView) findViewById(R.id.txt_anak_client_count);
+        kohortKbCountView = (TextView) findViewById(R.id.txt_kohort_kb_register_count);
     }
 
     @Override
@@ -25,6 +42,27 @@ public class BidanLandingActivity extends AppCompatActivity{
         super.onPause();
     }
 
+    // BaseActivity
+//    @Override
+//    protected int getContentView() {
+//        return R.layout.bidan_landing;
+//    }
+
+//    @Override
+//    protected int getDrawerLayoutId() {
+//        return R.id.drawer_layout;
+//    }
+//
+//    @Override
+//    protected int getToolbarId() {
+//        return LocationSwitcherToolbar.TOOLBAR_ID;
+//    }
+
+//    @Override
+//    protected Class onBackActivity() {
+//        return null;
+//    }
+//
     @Override
     protected void onResume() {
         super.onResume();
@@ -34,4 +72,5 @@ public class BidanLandingActivity extends AppCompatActivity{
     protected void onDestroy() {
         super.onDestroy();
     }
+
 }
